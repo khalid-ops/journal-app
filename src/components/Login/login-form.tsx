179c9f870e47fedfb06fbfb52ca5031b90caa3b5
khalid_shaikh <khalid.shaik@oryx.edu.qa>
2025-07-11 20:02:39 +0300

    image path change

--- src/components/Login/login-form.tsx
+++ src/components/Login/login-form.tsx
@@ -88,7 +88,7 @@ export function LoginForm({
           </form>
           <div className="bg-muted relative hidden md:block">
             <img
-              src="/public/login-form-image.png"
+              src="./login-form-image.png"
               alt="Image"
               className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
             />
